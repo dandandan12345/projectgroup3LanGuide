b8824d7756b09275d81550ca41bf53c1c0eada1b
Alwan95 <52132413+Alwan95@users.noreply.github.com>
2020-12-20 19:57:55 +0100

    delete

--- app/src/main/java/com/example/project2/LevelEnum.java
+++
@@ -1,5 +0,0 @@
-package com.example.project2;
-
-public enum LevelEnum {
-    Beginner,Intermediate,Advanced
-}
--- app/src/main/java/com/example/project2/Skill.java
+++
@@ -1,6 +0,0 @@
-package com.example.project2;
-
-public  enum Skill {
-    Reading,Writing, Listening, Vocabulary
-}
-
